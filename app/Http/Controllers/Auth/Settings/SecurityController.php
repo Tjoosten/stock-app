955c9e8c777f79651ec97918378b2d27e72d9a5a
Tjoosten <Topairy@gmail.com>
2020-01-12 08:59:50 +0100

    complete tests for account security

--- app/Http/Controllers/Auth/Settings/SecurityController.php
+++ app/Http/Controllers/Auth/Settings/SecurityController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\Users\Settings\SecurityFormRequest;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class SecurityController extends Controller
 {
@@ -22,6 +24,11 @@ class SecurityController extends Controller
 
     public function update(SecurityFormRequest $request): RedirectResponse
     {
-        $request->merge(['password' => bcrypt($request->password)]);
+        DB::transaction(static function () use($request): void {
+            $request->user()->update(['password' => Hash::make($request->password)]);
+            flash('De beveiligings instellingen van je account zijn aangepast.', 'success');
+        });
+
+        return redirect()->route('account.security');
     }
 }
--- tests/Feature/Account/SecuritySettingsTest.php
+++ tests/Feature/Account/SecuritySettingsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Account;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\Helpers\InteractsWithFlashMessages;
 use Tests\TestCase;
 
@@ -41,29 +42,55 @@ class SecuritySettingsTest extends TestCase
             ->patch(route('account.security.update', $data))
             ->assertSessionHasErrorsIn('default', 'current_password');
 
-        $this->assertEquals(session('errors')->get('current_password')[0] , __('validation.matchUserPassword'));
+        $this->assertEquals(session('errors')->get('current_password')[0], __('validation.matchUserPassword'));
     }
 
     /** @test */
     public function updateSecuritySettingsConfirmationCheckFails(): void
     {
+        $taylor = factory(User::class)->create();
+        $data = ['current_password' => $taylor->password, 'password' => 'password', 'password_confirmation' => 'faultyPassword'];
 
+        $this->actingAs($taylor)
+            ->assertAuthenticatedAs($taylor)
+            ->patch(route('account.security.update', $data))
+            ->assertSessionHasErrorsIn('default', 'password');
+
+        $this->assertEquals(session('errors')->get('password')[0], __('validation.confirmed', ['attribute' => 'password']));
     }
      /** @test */
     public function updateSecuritySettingsWhenNoAuthentication(): void
     {
+        $data = ['current_password' => 'current_password', 'password' => 'password', 'password_confirmation' => 'password'];
 
+        $this->patch(route('account.security.update', $data))
+            ->assertRedirect(route('login'));
     }
 
     /** @test */
     public function updateSecuritySettingsSuccess(): void
     {
+        $lena = factory(User::class)->create();
+        $data = ['current_password' => 'password', 'password' => 'rootroot', 'password_confirmation' => 'rootroot'];
 
+        $this->actingAs($lena)
+            ->assertAuthenticatedAs($lena)
+            ->patch(route('account.security.update', $data))
+            ->assertRedirect(route('account.security'));
+
+        $this->assertTrue(Hash::check($data['password'], auth()->user()->getAuthPassword()));
+        $this->flashMessagesForLevel('success');
     }
 
     /** @test */
     public function updateSucritySettingsValidationErrors(): void
     {
+        $taylor = factory(User::class)->create();
 
+        $this->actingAs($taylor)
+            ->assertAuthenticatedAs($taylor)
+            ->patch(route('account.security.update', []))
+            ->assertSessionHasErrors(['current_password', 'password'])
+            ->assertStatus(302);
     }
 }
